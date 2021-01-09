1bcde1c7cf9a4afe452139ae6b485d9e650bd8fa
Mitchell Vitez <mitchell@mercury.co>
2021-01-08 23:42:00 -0500

    cleanup and remove sample exe

--- changelog.md
+++ changelog.md
@@ -1,5 +1,9 @@
 # ChangeLog
 
+## 0.3.0
+
+Use the new CPython.Simple API
+
 ## 0.2.0
 
 Update cpython version to be >= 3.5
--- test/Main.hs
+++ test/Main.hs
@@ -13,3 +13,25 @@ main = hspec $ do
     it "runs" $ do
       initialize
       pause 1.0
+
+    it "sample program" $ do
+      putStrLn "3 seconds until beeping begins"
+      sleep 3
+      forM_ [1..100] $ \i -> do
+        write "beep"
+        press [key|enter|]
+      sleep 0.5
+
+      {- # The above is equivalent to this Python code:
+
+      import pyautogui
+      import time
+
+      print('3 seconds until beeping begins')
+      time.sleep(3)
+
+      for i in range(1, 101):
+          pyautogui.write('beep')
+          pyautogui.press('enter')
+          time.sleep(0.5)
+      -}
